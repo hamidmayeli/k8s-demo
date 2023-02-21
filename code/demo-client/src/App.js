2a3014b8c32c1d147c89bad88b5cf568fa1cf001
Hamid Mayeli <hamid.mayeli@justeattakeaway.com>
2023-02-21 15:39:51 +0000

    Add codes

---
+++ code/demo-client/src/App.js
@@ -0,0 +1,40 @@
+import './App.css';
+import {useEffect, useState} from 'react';
+
+// http://localhost:5212/WeatherForecast
+
+function App() {
+  const [state, setState] = useState([]);
+
+  useEffect(() =>
+  {
+    fetch('/WeatherForecast')
+    .then(data =>{
+      data
+      .json()
+      .then(json =>{
+        console.log({json});
+        setState(json);
+      });
+    });
+  }, []);
+  
+  return (
+    <div className="App">
+      <h1>
+        The K8s Demo Client
+      </h1>
+      <p>
+        <ul>
+          {state.map(item =>(
+            <li>
+              {item.date} <strong>{item.temperatureC}</strong>({item.summary})
+            </li>
+          ))}
+        </ul>
+        </p>
+    </div>
+  );
+}
+
+export default App;
